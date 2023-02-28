c5a1b855abaf3a1c452d9cfaaa1dfe06b7345b9c
daniel <pd22091988@gmail.com>
2023-02-28 13:45:36 +0500

    Add PSQL sites_table

--- migrations/20230227161320-init-management-site.sql
+++ migrations/20230227161320-init-management-site.sql
@@ -9,24 +9,8 @@ CREATE TABLE sites
     changed_at timestamp DEFAULT current_timestamp
 );
 
--- +migrate StatementBegin
 
-CREATE OR REPLACE FUNCTION update_modified_column()
-    RETURNS TRIGGER AS
-$$
-BEGIN
-    NEW.changed_at = now();
-    RETURN NEW;
-END;
-$$ language 'plpgsql';
-
--- +migrate StatementEnd
-
-
-CREATE TRIGGER update_sites_modtime BEFORE UPDATE ON sites FOR EACH ROW EXECUTE PROCEDURE  update_modified_column();
 
 -- +migrate Down
 DROP TABLE sites;
-DROP FUNCTION update_modified_column;
-DROP TRIGGER update_sites_modtime ON sites;
 DROP EXTENSION "uuid-ossp";
